1d34f74114d5b4f0f15aff74879759fd5752685c
Jun <fabio.jun@gmail.com>
2021-12-23 19:25:56 -0300

    -Fix header menu layout

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -56,7 +56,6 @@ const Nav = ( {slug} ) => {
 					<div className="text-sm font-medium">
 						<a href="#responsive-header" className="block mt-4 lg:inline-block lg:mt-0 text-black hover:text-black mr-10">
 						<svg xmlns="http://www.w3.org/2000/svg" className="hidden lg:block m-auto" fill="none" viewBox="0 0 24 24" width="18" height="auto" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z" /></svg>
-							Entrar
 						</a>
 						<CartIcon/>
 					</div>
--- src/components/cart/CartIcon.js
+++ src/components/cart/CartIcon.js
@@ -11,10 +11,11 @@ const CartIcon = () => {
 	return (
 		<Link href="/cart">
 			<a className="block mt-4 lg:inline-block lg:mt-0 text-black hover:text-black mr-10">
+				<span className='flex'>
 				<svg xmlns="http://www.w3.org/2000/svg" className="hidden lg:block m-auto" fill="none" viewBox="0 0 24 24" width="18" height="auto" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M16 11V7a4 4 0 00-8 0v4M5 9h14l1 12H4L5 9z" /></svg>
-				Carrinho
-				{ productsCount ? <span className="ml-1">({ productsCount })</span> : '' }
+				{ productsCount ? <span>({ productsCount })</span> : '' }
 				{/*{ totalPrice ? <span>{ totalPrice }</span> : '' }*/}
+				</span>
 			</a>
 		</Link>
 
