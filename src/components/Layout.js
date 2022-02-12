87f4bdec3709f7de9f4b1f2fd3d72b8ef7ba4bd4
Jun <fabio.jun@gmail.com>
2022-02-12 19:46:22 -0300

    -Fix google fonts, moved to custom document

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -59,10 +59,6 @@ const Layout = ({ data, isPost, children }) => {
 							rel="shortcut icon"
 							href={header?.favicon}
 						/>
-						<link 
-							href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,400;0,700;1,400;1,700&display=swap" 
-							rel="stylesheet" 
-						/>
 						{seo?.schemaDetails
 							? (
 								<script
