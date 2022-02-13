69cdb95b8e8f365bcb6e1c3661d399ce6ce0a56b
Jun <fabio.jun@gmail.com>
2022-02-13 00:50:34 -0300

    -Add jest html loader

--- jest.config.js
+++ jest.config.js
@@ -13,6 +13,15 @@ const customJestConfig = {
     // if using TypeScript with a baseUrl set to the root directory then you need the below for alias' to work
     moduleDirectories: ['node_modules', '<rootDir>/'],
     testEnvironment: 'jest-environment-jsdom',
+    moduleFileExtensions: [
+        "js",
+        "html"
+    ],
+    transform: {
+        "^.+\\.js$": "babel-jest",
+        "^.+\\.html?$": "html-loader-jest"
+    }
+
 };
 
 // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
