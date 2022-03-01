d19c7a89d8f0162ee68aa62a6b7e0451bd2f6e95
Fabio Jun <fabiojundev@gmail.com>
2022-02-28 22:21:35 -0300

    Add jest config to ignore cypress folder

--- jest.config.js
+++ jest.config.js
@@ -12,6 +12,7 @@ const customJestConfig = {
     // setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
     // if using TypeScript with a baseUrl set to the root directory then you need the below for alias' to work
     moduleDirectories: ['node_modules', '<rootDir>/'],
+    modulePathIgnorePatterns: ["cypress"],
     testEnvironment: 'jest-environment-jsdom',
     moduleFileExtensions: [
         "js",
