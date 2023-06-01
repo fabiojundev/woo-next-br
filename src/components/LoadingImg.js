d911e6139fef16e5f73ff51bf83400b42e7bd0dd
Fabio Jun <fabiojundev@gmail.com>
2023-05-31 23:45:39 -0300

    Fix loading position

--- src/components/LoadingImg.js
+++ src/components/LoadingImg.js
@@ -1,18 +1,17 @@
 import Image from "./image";
 
-
 const LoadingImg = (props) => {
-
-	return (
-		<span className="align-middle" {...props} >
-			<Image 
-				src='/cart-spinner.gif' 
-				width="54px" 
-				height="54px" 
-				alt="Carregando..."
-			/>
-		</span>
-	);
+  return (
+    <span className="align-middle h-full inline-block" {...props}>
+      <Image
+        src="/cart-spinner.gif"
+        width="54px"
+        height="54px"
+        alt="Carregando..."
+        className={"mt-4"}
+      />
+    </span>
+  );
 };
 
 export default LoadingImg;
